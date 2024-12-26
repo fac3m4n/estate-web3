9b7d4d8ee34682efb524175e27f73ba2c2d7dd49
fac3m4n <akerimberdi@gmail.com>
2024-12-26 14:55:35 +0200

    typo

--- packages/hardhat/deploy/00_deploy_tusdc.ts
+++ packages/hardhat/deploy/00_deploy_tusdc.ts
@@ -8,7 +8,7 @@ import { Contract } from "ethers";
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployTusdc: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
@@ -37,8 +37,8 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   console.log("👋 Initial greeting:", await tusdc.balanceOf(deployer));
 };
 
-export default deployYourContract;
+export default deployTusdc;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["tUSDC"];
+deployTusdc.tags = ["tUSDC"];
